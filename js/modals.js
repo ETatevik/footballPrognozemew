2cae3bcfce624b335dcc0121d3a9e47874c04a9c
eTatev <barev.tatevik@gmail.com>
2019-11-29 01:25:27 +0400

    Only News Slider

--- js/modals.js
+++ js/modals.js
@@ -95,18 +95,16 @@ jQuery(document).ready(function($) {
 			var send = true;
 			jQuery('#registration-modal input').each(function() {
 				send  = check.call(this);
-				console.log(send)
-
 			});
-			if(jQuery('.modal#registration-modal form .select-selected').text() == document.querySelector('#registration-modal .custom-select > select > option[value="0"]').innerHTML ){
-				jQuery('.modal#registration-modal form .select-selected').addClass('error');
-				jQuery('.modal#registration-modal form .select-selected').parent('.custom-select').next('.invalid').addClass('error');
+			if(jQuery('.select-body input[name="chooseTarif"]:checked').length == 0){
+				jQuery('#registration-modal .select-form .show-select .choose-radio').addClass('error');
+				jQuery('.select-form .invalid').addClass('error');
 				send = false;
 			}else{
-				send = true;
+				send = checkValuePasswordsConfPass(jQuery('#regPassMD'),jQuery('#regPassConfMD'));
+				jQuery('#registration-modal .select-form .show-select .choose-radio').removeClass('error').addClass('active');
+				jQuery('.select-form .invalid').removeClass('error');
 			}
-			send = checkValuePasswordsConfPass(jQuery('#regPassMD'),jQuery('#regPassConfMD'));
-
 			return send;
 		});
 	}
@@ -140,6 +138,24 @@ jQuery(document).ready(function($) {
 			jQuery('.modal#subscription-modal').fadeOut(0);
 			jQuery('.modal#login-modal').fadeIn(500).css('display','flex');
 			jQuery('body').css('overflow-y' , 'hidden');
+		});
+	}
+
+	// select in registration
+	{
+		var chooseTarifDefaultText = jQuery('#registration-modal .select-form .show-select .choose-radio').text();
+		jQuery('#registration-modal .select-form .select-body').slideUp(0);
+		jQuery('#registration-modal .select-form .show-select').click(function(event) {
+			jQuery('#registration-modal .select-form .select-body').slideToggle(500);
+			if(chooseTarifDefaultText == jQuery('#registration-modal .select-form .show-select .choose-radio').text())
+				jQuery(this).children('.choose-radio').toggleClass('active');
+			else
+				jQuery(this).children('.choose-radio').addClass('active');
+		});
+
+		jQuery('#registration-modal .select-form .select-body label').click(function(event) {
+			jQuery('#registration-modal .select-form .show-select .choose-radio').html(jQuery(this).children('.select-text').html());
+			jQuery('#registration-modal .select-form .select-body').slideUp(500);
 		});
 	}
 
@@ -178,6 +194,7 @@ function check(){
 		jQuery(this).parent('label').next('.invalid').removeClass('error');
 		send = true;
 	}
+
 	return send;
 }
 
@@ -209,84 +226,4 @@ function checkValuePasswordsConfPass(pass, confPass){
 		confPass.parent('label').next('.invalid').removeClass('error');
 	}
 	return send;
-}
-
-// select tarif 
-{
-	var x, i, j, selElmnt, a, b, c;
-	/*look for any elements with the class "custom-select":*/
-	x = document.querySelectorAll(".custom-select");
-	for (i = 0; i < x.length; i++) {
-	  selElmnt = x[i].getElementsByTagName("select")[0];
-	  /*for each element, create a new DIV that will act as the selected item:*/
-	  a = document.createElement("DIV");
-	  a.setAttribute("class", "select-selected");
-	  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-	  x[i].appendChild(a);
-	  /*for each element, create a new DIV that will contain the option list:*/
-	  b = document.createElement("DIV");
-	  b.setAttribute("class", "select-items select-hide");
-	  for (j = 1; j < selElmnt.length; j++) {
-	    /*for each option in the original select element,
-	    create a new DIV that will act as an option item:*/
-	    c = document.createElement("DIV");
-	    c.innerHTML = makeHtmlText(selElmnt.options[j]);
-	    
-	    c.addEventListener("click", function(e) {
-	        /*when an item is clicked, update the original select box,
-	        and the selected item:*/
-	        var y, i, k, s, h;
-	        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-	        h = this.parentNode.previousSibling;
-	        for (i = 0; i < s.length; i++) {
-	          if (makeHtmlText(s.options[i]) == this.innerHTML) {
-	            s.selectedIndex = i;
-	            h.innerHTML = this.innerHTML;
-	            y = this.parentNode.getElementsByClassName("same-as-selected");
-	            for (k = 0; k < y.length; k++) {
-	              y[k].removeAttribute("class");
-	            }
-	            this.setAttribute("class", "same-as-selected");
-	            if(document.querySelector('#registration-modal .custom-select > select > option[value="0"]').innerHTML != this.innerHTML) this.parentNode.previousSibling.classList.add('active');
-	            break;
-	          }
-	        }
-	        h.click();
-	    });
-	    b.appendChild(c);
-	  }
-	  x[i].appendChild(b);
-	  a.addEventListener("click", function(e) {
-	      /*when the select box is clicked, close any other select boxes,
-	      and open/close the current select box:*/
-	      e.stopPropagation();
-	      closeAllSelect(this);
-	      this.nextSibling.classList.toggle("select-hide");
-	      this.classList.toggle("select-arrow-active");
-	    });
-	}
-	function closeAllSelect(elmnt) {
-	  /*a function that will close all select boxes in the document,
-	  except the current select box:*/
-	  var x, y, i, arrNo = [];
-	  x = document.getElementsByClassName("select-items");
-	  y = document.getElementsByClassName("select-selected");
-	  for (i = 0; i < y.length; i++) {
-	    if (elmnt == y[i]) {
-	      arrNo.push(i)
-	    } else {
-	      y[i].classList.remove("select-arrow-active");
-	    }
-	  }
-	  for (i = 0; i < x.length; i++) {
-	    if (arrNo.indexOf(i)) {
-	      x[i].classList.add("select-hide");
-	    }
-	  }
-	}
-
-	function makeHtmlText(elem){
-	  var text = elem.innerHTML.split('w');
-	  return text[0] + '<strong>' + text[1] + '</strong>'+"<span>" + text[2] + "</span>";
-	}
 }
--- js/user.js
+++ js/user.js
@@ -55,83 +55,21 @@ jQuery(document).ready(function($) {
 		}
 	}
 
-	// select tarif 
+	// select tarif in user Settings 
 	{
-		var x, i, j, selElmnt, a, b, c;
-		/*look for any elements with the class "custom-select":*/
-		x = document.querySelectorAll(".custom-select");
-		for (i = 0; i < x.length; i++) {
-		  selElmnt = x[i].getElementsByTagName("select")[0];
-		  /*for each element, create a new DIV that will act as the selected item:*/
-		  a = document.createElement("DIV");
-		  a.setAttribute("class", "select-selected");
-		  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-		  x[i].appendChild(a);
-		  /*for each element, create a new DIV that will contain the option list:*/
-		  b = document.createElement("DIV");
-		  b.setAttribute("class", "select-items select-hide");
-		  for (j = 1; j < selElmnt.length; j++) {
-		    /*for each option in the original select element,
-		    create a new DIV that will act as an option item:*/
-		    c = document.createElement("DIV");
-		    c.innerHTML = makeHtmlText(selElmnt.options[j]);
-		    
-		    c.addEventListener("click", function(e) {
-		        /*when an item is clicked, update the original select box,
-		        and the selected item:*/
-		        var y, i, k, s, h;
-		        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-		        h = this.parentNode.previousSibling;
-		        for (i = 0; i < s.length; i++) {
-		          if (makeHtmlText(s.options[i]) == this.innerHTML) {
-		            s.selectedIndex = i;
-		            h.innerHTML = this.innerHTML;
-		            y = this.parentNode.getElementsByClassName("same-as-selected");
-		            for (k = 0; k < y.length; k++) {
-		              y[k].removeAttribute("class");
-		            }
-		            this.setAttribute("class", "same-as-selected");
-		            if(document.querySelector('#userDataSetting .custom-select > select > option[value="0"]').innerHTML != this.innerHTML) this.parentNode.previousSibling.classList.add('active');
-		            break;
-		          }
-		        }
-		        h.click();
-		    });
-		    b.appendChild(c);
-		  }
-		  x[i].appendChild(b);
-		  a.addEventListener("click", function(e) {
-		      /*when the select box is clicked, close any other select boxes,
-		      and open/close the current select box:*/
-		      e.stopPropagation();
-		      closeAllSelect(this);
-		      this.nextSibling.classList.toggle("select-hide");
-		      this.classList.toggle("select-arrow-active");
-		    });
-		}
-		function closeAllSelect(elmnt) {
-		  /*a function that will close all select boxes in the document,
-		  except the current select box:*/
-		  var x, y, i, arrNo = [];
-		  x = document.getElementsByClassName("select-items");
-		  y = document.getElementsByClassName("select-selected");
-		  for (i = 0; i < y.length; i++) {
-		    if (elmnt == y[i]) {
-		      arrNo.push(i)
-		    } else {
-		      y[i].classList.remove("select-arrow-active");
-		    }
-		  }
-		  for (i = 0; i < x.length; i++) {
-		    if (arrNo.indexOf(i)) {
-		      x[i].classList.add("select-hide");
-		    }
-		  }
-		}
+		var chooseTarifDefaultText = jQuery('#userDataSetting .select-form .show-select .choose-radio').text();
+		jQuery('#userDataSetting .select-form .select-body').slideUp(0);
+		jQuery('#userDataSetting .select-form .show-select').click(function(event) {
+			jQuery('#userDataSetting .select-form .select-body').slideToggle(500);
+			if(chooseTarifDefaultText == jQuery('#userDataSetting .select-form .show-select .choose-radio').text())
+				jQuery(this).children('.choose-radio').toggleClass('active');
+			else
+				jQuery(this).children('.choose-radio').addClass('active');
+		});
 
-		function makeHtmlText(elem){
-		  var text = elem.innerHTML.split('w');
-		  return text[0] + '<strong>' + text[1] + '</strong>'+"<span>" + text[2] + "</span>";
-		}
+		jQuery('#userDataSetting .select-form .select-body label').click(function(event) {
+			jQuery('#userDataSetting .select-form .show-select .choose-radio').html(jQuery(this).children('.select-text').html());
+			jQuery('#userDataSetting .select-form .select-body').slideUp(500);
+		});
 	}
 });
